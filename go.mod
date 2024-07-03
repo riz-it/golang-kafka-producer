4bc43a4db93171053e4df8316dae6451a2fa09d8
riz.it <mrizalf.email@gmail.com>
2024-07-03 17:51:40 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-kafka-producer
+
+go 1.22.4
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main () {
+	config := &kafka.ConfigMap{
+		"bootstrap.servers" : "localhost:9092",
+	}
+
+	producer, err := kafka.NewProducer(config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer producer.Close()
+
+	topic := "helloworld"
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Send message to kafka %d \n", i)
+
+		key := strconv.Itoa(i)
+		value := fmt.Sprintf("Hello %d", i)
+
+		msg := &kafka.Message{
+			TopicPartition: kafka.TopicPartition{
+				Topic: &topic,
+				Partition: kafka.PartitionAny,
+			},
+			Key: []byte(key),
+			Value: []byte(value),
+		}
+
+		err := producer.Produce(msg, nil)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	producer.Flush(5 * 1000)
+}
